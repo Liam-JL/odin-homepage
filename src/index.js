de4d0638a52da374c37a9934487901be31fbcd02
LiamJL <paledeepened@gmail.com>
2025-07-13 16:19:20 +0100

    Generate project cards and render on site load

--- src/index.js
+++ src/index.js
@@ -3,3 +3,8 @@ import "./shared/styles/header.css"
 import "./shared/styles/page-layout.css"
 import "./shared/styles/main.css"
 import "./shared/styles/footer.css"
+import { renderProjectCardList } from "./widgets/project-card-list"
+
+window.onload = () => {
+    renderProjectCardList()
+}
