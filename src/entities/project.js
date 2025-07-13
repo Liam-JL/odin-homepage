84e1372788c2f8b30974849a11a15e3d10c6e871
LiamJL <paledeepened@gmail.com>
2025-07-13 16:18:54 +0100

    Change project storage to hardcoded array

--- src/entities/project.js
+++
@@ -1,9 +0,0 @@
-export class Project {
-    constructor(title, description, screenshotPath, githubLink, siteLink) {
-        this.title = title;
-        this.description = description;
-        this.screenshotPath = screenshotPath;
-        this.githubLink = githubLink;
-        this.siteLink = siteLink;
-    }
-}
