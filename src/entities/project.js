18ccd03fd6f92e0612097d7d14efb54945ad7bb7
LiamJL <paledeepened@gmail.com>
2025-07-13 12:03:43 +0100

    Create array of projects to use as examples on page

--- src/entities/project.js
+++ src/entities/project.js
@@ -1,4 +1,4 @@
-class Project {
+export class Project {
     constructor(title, description, screenshotPath, githubLink, siteLink) {
         this.title = title;
         this.description = description;
---
+++ src/entities/projectStorage.js
@@ -0,0 +1,45 @@
+import { Project } from "./project";
+
+export let projectArray = [];
+
+projectArray.push(new Project("Odin Todo",
+    "A functional Todo List application built as part of The Odin Project curriculum.",
+    "https://github.com/Liam-JL/odin-todo/raw/main/Screenshots/odintodoss.png",
+    "https://github.com/Liam-JL/odin-todo",
+    "https://liam-jl.github.io/odin-todo/"
+ ))
+
+ projectArray.push(new Project("Odin Weather",
+    "A simple JavaScript weather application that fetches real-time weather data for any city using the Visual Crossing weather API",
+    "https://github.com/Liam-JL/odin-weather/raw/main/odinweatherss.png",
+    "https://github.com/Liam-JL/odin-weather",
+    "https://liam-jl.github.io/odin-weather/"
+ ))
+
+projectArray.push(new Project("Odin Library",
+    "A simple Book Library App. It allows users to add books with details like title, author, shelf category, rating, and review.",
+    "https://images.unsplash.com/photo-1610116306796-6fea9f4fae38?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://github.com/Liam-JL/odin-library",
+    "https://liam-jl.github.io/odin-library/"
+))
+
+projectArray.push(new Project("Odin Tic Tac Toe",
+    "This project is a web-based Tic-Tac-Toe game built in JavaScript, HTML, and CSS.",
+    "https://images.unsplash.com/photo-1668901382969-8c73e450a1f5?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://github.com/Liam-JL/odin-tic-tac-toe",
+    "https://liam-jl.github.io/odin-tic-tac-toe/"
+))
+
+projectArray.push(new Project("Odin Battleship",
+    "This is a browser-based implementation of the classic Battleship game, built as part of The Odin Project's JavaScript curriculum.",
+    "https://github.com/Liam-JL/odin-battleship/raw/main/battleship-ss.png",
+    "https://github.com/Liam-JL/odin-battleship",
+    "https://liam-jl.github.io/odin-battleship/"
+))
+
+projectArray.push(new Project("Odin Admin Dashboard",
+    "A responsive dashboard created using HTML, CSS, and JavaScript.",
+    "https://github.com/Liam-JL/odin-admin-dashboard/raw/main/Images/odin-dashboard.png",
+    "https://github.com/Liam-JL/odin-admin-dashbo",
+    "https://liam-jl.github.io/odin-admin-dashboard/"
+))
