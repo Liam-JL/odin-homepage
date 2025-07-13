dc6cb4064a0c6848e6c6e2afb3c550d6380d1363
LiamJL <paledeepened@gmail.com>
2025-07-13 10:21:10 +0100

    Implement project class

---
+++ src/entities/project.js
@@ -0,0 +1,9 @@
+class Project {
+    constructor(title, description, screenshotPath, githubLink, siteLink) {
+        this.title = title;
+        this.description = description;
+        this.screenshotPath = screenshotPath;
+        this.githubLink = githubLink;
+        this.siteLink = siteLink;
+    }
+}
