03b769cd65520d67fef327937cd0dc6d0254fecd
LiamJL <paledeepened@gmail.com>
2025-07-14 21:38:56 +0100

    Implement project card list desktop responsiveness

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import "./shared/styles/header.css"
 import "./shared/styles/background.css"
 import "./shared/styles/main.css"
 import "./shared/styles/footer.css"
-import "./shared/styles/responsiveness.css"
+import "./shared/styles/responsiveness-tablet.css"
+import "./shared/styles/responsiveness-desktop.css"
 import { renderProjectCardList } from "./widgets/project-card-list"
 
 window.onload = () => {
