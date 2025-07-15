a047c1430311a7a57389328672efe47d1a83a8ab
LiamJL <paledeepened@gmail.com>
2025-07-15 15:26:13 +0100

    Add transitions to icons when hovered

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import "./shared/styles/main.css"
 import "./shared/styles/footer.css"
 import "./shared/styles/responsiveness-tablet.css"
 import "./shared/styles/responsiveness-desktop.css"
+import "./shared/styles/animations.css"
 import { renderProjectCardList } from "./widgets/project-card-list"
 
 window.onload = () => {
